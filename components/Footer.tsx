0cff0ef0d924ff62ce17f51421ccdad26b334d19
Avan Kel <promisewonder@gmail.com>
2025-11-04 12:48:37 +0100

    Initial deploy-ready build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-darkGray text-white py-16 mt-20">
       <div className="max-w-6xl mx-auto px-8 text-center">
-        <h2 className="text-red">
+        <h2 className="text-red-500">
           Side note to reviewer: I&apos;m sorry I&apos;m not able to complete these projects including the ToDo list
           application within the given time and my multitasking with other things I&apos;m working on.
           Don&apos;t waste too much energy but give it a good look. I&apos;m really sorry!
